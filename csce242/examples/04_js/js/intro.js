61587b632cb8a3ced3cd14fdb66af561232dbb63
Daniel Moss <danielpmoss123@gmail.com>
2025-09-11 12:53:39 -0400

    in class 9-11-2025

--- csce242/examples/04_js/js/intro.js
+++ csce242/examples/04_js/js/intro.js
@@ -4,8 +4,37 @@ const sayHello = () => {
 }
 */
 
-document.getElementById("btn-click-me").onclick = sayHello;
+//document.getElementById("btn-click-me").onclick = sayHello;
 
-document.getElementById("btn-click-me").onclick = () => {
-    console.log("hello world");
+document.getElementById("btn-click-me").onclick = (event) => {
+    document.getElementById("p-welcome").innerHTML = "Hello World";
+    event.currentTarget.classList.add("clicked");   //current traget is the button that was clicked
+}
+
+document.getElementById("happy-button").onclick = () => {
+    const pChange = document.getElementById("p-change");
+    pChange.innerHTML = "Happy!";
+    pChange.classList.add("yellow");
+    pChange.classList.remove("blue");
+}
+
+document.getElementById("sad-button").onclick = () => {
+    const pChange = document.getElementById("p-change");
+    pChange.innerHTML = "Sad :(";
+    pChange.classList.add("blue");
+    pChange.classList.remove("yellow");
+}
+
+document.getElementById("clear-button").onclick = () => {
+    const pChange = document.getElementById("p-change");
+    pChange.innerHTML = "";
+    pChange.classList.remove("blue");
+    pChange.classList.remove("yellow");
+}
+
+document.getElementById("txt-emotion").onkeyup = (event) => {
+    const userInput = event.currentTarget.value;
+    document.getElementById("p-emotion").innerHTML = `You are feeling ${userInput}.`
+    document.getElementById("img-emotion").classList.remove("hidden");
+    //.style
 }
