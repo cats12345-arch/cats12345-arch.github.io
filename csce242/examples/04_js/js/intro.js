2766b8279fbdb1ef283575f0f42bd21b64aa94a2
Daniel Moss <danielpmoss123@gmail.com>
2025-09-18 13:16:24 -0400

    In class 9-18-2025

--- csce242/examples/04_js/js/intro.js
+++ csce242/examples/04_js/js/intro.js
@@ -85,3 +85,76 @@ document.getElementById("btn-bounce").onclick = (event) => {
     ball.classList.toggle("bounce");
 }
 //On chnage
+
+/* Counter */
+let counter = 0;
+let counterInterval;
+const countP = document.getElementById("p-count");
+const btnStart = document.getElementById("btn-count-start");
+const btnStop = document.getElementById("btn-count-stop");
+const btnReset = document.getElementById("btn-count-reset");
+btnStop.disabled = true;
+btnReset.disabled = true;
+
+document.getElementById("btn-count-start").onclick = () => {
+    btnStop.disabled = false;
+    btnReset.disabled = false;
+    btnStart.disabled = true;
+    counterInterval = setInterval(()=>{
+        counter++;
+        countP.innerHTML = counter;
+    }, 1000);
+}
+
+document.getElementById("btn-count-stop").onclick = () => {
+    clearInterval(counterInterval);
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+}
+
+document.getElementById("btn-count-reset").onclick = () => {
+    counter = 0;
+    clearInterval(counterInterval);
+    countP.innerHTML = counter;
+    btnStop.disabled = true;
+    btnReset.disabled = true;
+    btnStart.disabled = false;
+}
+
+/* Donation stuff */
+const goal = 10000;
+document.getElementById("goal-span").innerHTML = goal;
+
+document.getElementById("btn-donations").onclick = () => {
+    const donation = document.getElementById("txt-donations").value;
+    const errorSpan = document.getElementById("donation-error");
+    const pPercentage = document.getElementById("percentage");
+    errorSpan.innerHTML = "";
+    pPercentage.innerHTML = "";
+
+    if(isNaN(donation) || donation <= 0) {
+        errorSpan.innerHTML = "* Invalid Amount."
+        return;
+    }
+
+    const donationPercent = donation / goal * 100;
+
+    if(donationPercent >= 100) {
+        pPercentage.innerHTML = "Goal Reached";
+    }
+    else if(donationPercent >= 80) {
+        pPercentage.innerHTML = "Were almost there";
+    }
+    else if(donationPercent >= 50) {
+        pPercentage.innerHTML = "Over half way there";
+    }
+    else if(donationPercent >= 25) {
+        pPercentage.innerHTML = "Keep Going";
+    }
+    else {
+        pPercentage.innerHTML = "Lets get started";
+    }
+
+    document.querySelector(":root").style.setProperty("--donation-percent", donationPercent + "%");
+}
+
