e70d4f2996aaa6f30846de223af803383c1b2b01
Daniel Moss <danielpmoss123@gmail.com>
2025-09-21 21:55:57 -0400

    added the finishing touches

--- csce242/assignments/06javaConditionals/js/toggle-nav.js
+++ csce242/assignments/06javaConditionals/js/toggle-nav.js
@@ -20,37 +20,46 @@ document.getElementById("exercise-2").onclick = () => {
 document.getElementById("myRange").onclick = () => {
     let days = document.getElementById("myRange").value;
     const flower = document.getElementById("flower");
+    const numdays = document.getElementById("numberDays");
+    const status = document.getElementById("status");
     console.log(days);
     if(days <= 2) {
         flower.src="images/plant1.jpeg";
+        numdays.innerHTML=`It's been ${days} since watering your plant`;
+        status.innerHTML="your plant is healthy and happy";
     }
     else if(days <= 5) {
         flower.src="images/plant2.jpg";
+        numdays.innerHTML=`It's been ${days} since watering your plant`;
+        status.innerHTML="Your plant needs watering";
     }
     else if(days <= 9) {
         flower.src="images/plant3.jpg";
+        numdays.innerHTML=`It's been ${days} since watering your plant`;
+        status.innerHTML="Leaves are droppering the color is changing, water soon";
     }
     else {
         flower.src="images/plant4.png";
+        numdays.innerHTML=`It's been ${days} since watering your plant`;
+        status.innerHTML="Sorry, your plant is no longer with us";
     }
 }
 
 function updateTime() {
-    console.log("please work");
     const time = new Date();
-    let hour = new getHours();
-    const min = new getMinutes();
+    let hour = time.getHours();
+    const min = time.getMinutes();
     const pChange = document.getElementById("pChange");
     if(hour == 12) {
-        pChange.innerHTML = hour + ":" + min + " pm";
+        pChange.innerHTML = `${hour}:${min} pm`;
     }
     
     if(hour >= 13) {
         hour -= 12;
-        pChange.innerHTML = hour + ":" + min + " pm";
+        pChange.innerHTML = `${hour}:${min} pm`;;
     }
     else {
-        pChange.innerHTML = hour + ":" + min + " am";
+        pChange.innerHTML = `${hour}:${min} am`;
     }
 
     
