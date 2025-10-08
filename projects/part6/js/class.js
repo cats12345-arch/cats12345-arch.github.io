e302e78584e009397a34f3eed7d06d6a938e4b03
Daniel Moss <danielpmoss123@gmail.com>
2025-10-08 14:46:28 -0400

    minor changes to the json file

--- projects/part6/js/class.js
+++
@@ -1,9 +0,0 @@
-class Book {
-    constructor(id, name, author, price, releaseDate) {
-        this.id = id;
-        this.name = name;
-        this.author = author;
-        this.price = price;
-        this.releaseDate = releaseDate;
-    }
-}
