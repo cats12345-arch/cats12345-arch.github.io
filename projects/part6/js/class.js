4d13d052ae3342446afa2c501dfc313a2b066971
Daniel Moss <danielpmoss123@gmail.com>
2025-10-08 14:39:28 -0400

    in class examples plus updated json files

---
+++ projects/part6/js/class.js
@@ -0,0 +1,9 @@
+class Book {
+    constructor(id, name, author, price, releaseDate) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.releaseDate = releaseDate;
+    }
+}
